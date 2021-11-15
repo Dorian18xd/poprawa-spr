1c9bc0db64636b8c43cbedb3084d37450b9a40ea
Dorian18xd <93906692+Dorian18xd@users.noreply.github.com>
2021-11-15 08:52:29 +0100

    Add files via upload

---
+++ spr zad 1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+
+
+
+class Uczen {
+
+public:
+float imie;
+float nazwisko;
+float klase;
+float ocene_koncowa_z_jpolskiego;
+float ocene_koncowa_z_matematyki;
+float ocene_koncowa_z_fizyki;
+
+
+void dodajUcznia() 
+{
+	cout>>"Wpisz imie";
+	cin>>imie;
+	cout<<"Wpisz nazwisko";
+	cin>>nazwisko;
+	cout<<"Wpisz klase";
+	cin>>klase;
+	cout<<"Wpisz ocene koncowa z j.polskiego";
+	cin>>ocene_koncowa_z_jpolskiego;
+	cout<<"Wpisz ocene koncowa z matematyki";
+	cin>>ocene_koncowa_z_matematyki;
+	cout<<"Wpisz ocene koncowa z fizyki";
+	cin>>ocene_koncowa_z_fizyki;
+};
+
+wyswietlDane();
+wyswietlOceny();
+sredniaOcen();
+void metryczka(){
+	cout>>"Dorian Dul">>endl;
+}
+};
+
+int main()
+{
+	
+	
+	return metryczka;
+}
+
+
+
+
+
